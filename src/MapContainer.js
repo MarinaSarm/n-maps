0c234e223cd66924f80248ff2d4d981a3eaea56d
MarinaSarm <marsarman@yandex.ru>
2018-07-26 21:46:37 +0200

    add MapContainer component

---
+++ src/MapContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class MapContainer extends Component {
+  render() {
+    return(
+      <div id='map'></div>
+    )
+  }
+}
+export default MapContainer
