914cef93190c35174ef22da7fb135a27be92c577
MarinaSarm <marsarman@yandex.ru>
2018-07-27 18:14:31 +0200

    add map to map with react-google-maps

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <MapContainer />
+        <MapContainer
+          isMarkerShown={true}
+        />
       </div>
     );
   }
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
-
-class MapContainer extends Component {
-  render() {
-    return(
-      <div id='map'></div>
+import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps"
+import { compose, withProps } from 'recompose'
+const MapContainer = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAbAAsS7Hhe1k-bnddQpHAVoJ7rBJOzE_w&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => {
+    return (
+        <GoogleMap
+          defaultZoom={8}
+          defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        >
+          {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+        </GoogleMap>
     )
   }
-}
+)
+
+
 export default MapContainer
